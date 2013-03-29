20ab76bfc3e8c2ef1ee234fa7dbfc3f7e440f350
Zachary Zeilinger <zzeilinger@gmail.com>
2013-03-28 21:55:53 -0700

    Added checking for the srvlst.txt file in script
    If the srvlst.txt file is none existent in the directory that
    Systems-Status.ps1 is in the script will create the file and pause until
    the user has populated the file, saved, and closed the srvlst.txt  file.

--- SystemStauts.ps1
+++ SystemStauts.ps1
@@ -16,8 +16,12 @@
     Servername2 is up!
 
 .Notes
-    Requires a computer.txt file to be n the same directory as the .ps1 file.
+    Requires a srvlst.txt file to be n the same directory as the .ps1 file.
 #>
+
+#Turns off error messages
+$ErrorActionPreference = 'silentlycontinue'
+
 #Sets window name and size
 $a = (Get-Host).UI.RawUI
 $a.WindowTitle = "Systems-Status"
@@ -25,29 +29,46 @@ $b = $a.WindowSize
 $b.Width = 24
 $b.Height = 32
 $a.WindowSize = $b
+
 #Gets script location
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
-$Computers = Get-Content $dir\Computers.txt
+
+#Tests for text file with list of Exchange server names. If non exists in the directory the script will create the document open it and the user must then populate the list. 
+$path = "$dir\srvlst.txt"
+    if(!(Test-Path -path $path))
+        {
+            new-item -path $path -Value "Please populate srvlst.txt with server names. One per line then save and close" -itemtype file
+            Invoke-Item $dir\srvlst.txt
+            Write-Host "Press the any key to continue..."
+            $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+            cls
+        }
+            Else
+                {
+                    cls
+                }
+$srvlst = Get-Content "$dir\srvlst.txt"
+
 #Runs this section in an infinte loop until ctrl+c or the "X" is hit. This is where the real work happens!
 for (;;)
 { 
 $now=Get-Date -format HH:mm:ss #Gets the current time
 Write-Host "Ping ran at $now" -Foreground Magenta #writes the current time
-write-host
-Foreach ($ComputerName in $Computers) 
+write-host "--------------------" -Foreground Red
+Foreach ($s in $srvlst) 
     {
-        If (Test-Connection $ComputerName -count 1 -BufferSize 16 -TimeToLive 4 -Quiet)
+        If (Test-Connection $s -count 1 -BufferSize 16 -TimeToLive 4 -Quiet)
             {
-                Write-Host "$ComputerName is UP!" -ForegroundColor Green
+                Write-Host "$s is UP!" -ForegroundColor Green
             }
                 else
                     {
-                        Write-Host "$ComputerName is DOWN!" -ForegroundColor Red
+                        Write-Host "$s is DOWN!" -ForegroundColor Red
                     }
      }
 Write-Host "--------------------" -Foreground Red
-Start-Sleep -s 5
+Start-Sleep -s 3
 cls
 }
 
